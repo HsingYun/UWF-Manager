4cc111255e853d370addc0f7cf16719ab85c6c07
HsingYun <iakext@gmail.com>
2026-07-05 03:44:26 +0800

    style: apply clang-format

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2026 HsingYun (iakext@gmail.com)
+ * Copyright (c) 2026 HsingYun (iakext@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,8 +16,8 @@
  */
 #include "MainWindow.h"
 
-#include <windows.h>
 #include <dwmapi.h>
+#include <windows.h>
 
 #include <QAbstractItemView>
 #include <QAction>
